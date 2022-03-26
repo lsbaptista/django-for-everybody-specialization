f65ba739b5d8975423e247e7111e1a2d998b9d45
DaSilva <34317858+lsbaptista@users.noreply.github.com>
2022-03-26 12:46:50 +0000

    new

---
+++ Web-Application-Technologies-and-Django/webServer.py
@@ -0,0 +1,38 @@
+# %%
+# https://docs.python.org/3/howto/sockets.html
+# https://stackoverflow.com/questions/8627986/how-to-keep-a-socket-open-until-client-closes-it
+# https://stackoverflow.com/questions/10091271/how-can-i-implement-a-simple-web-server-using-python-without-using-any-libraries
+
+
+from socket import *
+
+def createServer():
+    serversocket = socket(AF_INET, SOCK_STREAM)
+    try :
+        serversocket.bind(('localhost',9000)) # he second one will blow up and say, you can't have port 9000 because another piece of software has it
+        serversocket.listen(5) #Dear operating system hold those temporarily. I'll get back to you.If there is more calls in the moment i am already receiving one
+        while(1):
+            (clientsocket, address) = serversocket.accept()
+
+            rd = clientsocket.recv(5000).decode()
+            pieces = rd.split("\n")
+            if ( len(pieces) > 0 ) : print(pieces[0])
+
+            data = "HTTP/1.1 200 OK\r\n"
+            data += "Content-Type: text/html; charset=utf-8\r\n" #network version of newline
+            data += "\r\n"
+            data += "<html><body>Hello World</body></html>\r\n\r\n"
+            clientsocket.sendall(data.encode())
+            clientsocket.shutdown(SHUT_WR) # client and server shutdown
+
+    except KeyboardInterrupt :
+        print("\nShutting down...\n");
+    except Exception as exc :
+        print("Error:\n");
+        print(exc)
+
+    serversocket.close()
+
+print('Access http://localhost:9000')
+createServer()
+# %%

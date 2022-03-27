f3087239140fd816f49f856bf630a26e532fa3f1
DaSilva <34317858+lsbaptista@users.noreply.github.com>
2022-03-27 11:31:43 +0100

    new

--- Web-Application-Technologies-and-Django/client1.py
+++ Web-Application-Technologies-and-Django/client1.py
@@ -3,7 +3,7 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('127.0.0.1', 9000))
+mysock.connect(('127.0.0.1' , 9000))
 cmd = 'GET http://127.0.0.1/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
